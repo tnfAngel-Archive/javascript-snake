d39853a4496e6bd780dd83f8b03735478c37229f
tnfAngel <57068341+tnfAngel@users.noreply.github.com>
2022-01-20 11:04:56 +0000

    Score changes

--- src/static/die/script.js
+++ src/static/die/script.js
@@ -2,6 +2,12 @@ function play() {
     window.location.href = '/play';
 }
 
-document.getElementById('ns').innerHTML = 'You loose';
+function getParameterByName(name) {
+    const match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
+
+
+document.getElementById('ns').innerHTML = getParameterByName('ns') === 'true' ? 'New score!' : 'You loose';
 document.getElementById('score').innerHTML = localStorage.getItem('points') || '0';
 document.getElementById('best_score').innerHTML = localStorage.getItem('score') || '0';
